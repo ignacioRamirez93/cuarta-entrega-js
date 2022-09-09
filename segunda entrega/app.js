dc7b6983bc96df988f01444b64a7712dfa889b22
ignacio Ramirez <ignacio-ramirez@hotmail.com.ar>
2022-09-08 21:41:01 -0300

    cuarta entrega

--- segunda entrega/app.js
+++ segunda entrega/app.js
@@ -1,146 +1,181 @@
-
-let ropa
+let ropa;
 let entrada;
-let total=0;
-let suma =0;
+let total = 0;
+let suma = 0;
+let resta = 0;
 let pago;
-let producto = [ "1 - Pantalon", "2 - Remera", "3 - Buzo","4 - Zapatillas"];
+let producto = [
+  { id: 1, tipo: "Pantalon", precio: 1500 },
+  { id: 2, tipo: "Remera", precio: 1000 },
+  { id: 3, tipo: "Buzo", precio: 2000 },
+  { id: 4, tipo: "Zapatillas", precio: 3000 },
+];
 const carrito = [];
-const listaproductos= producto.join("\n");
- 
+let productoaeliminar;
 
- 
- console.log (listaproductos);
+for (let tipo of producto) {
+  console.log(`ID Producto: ${tipo.id}`);
+  console.log(tipo.tipo);
+  console.log(tipo.precio);
+}
 
+do {
+  ropa = prompt("Ingrese el ID del producto que desea comprar");
 
-
-do{
-    ropa=prompt ( `Ingrese el numero de lo que quiere comprar:\n${listaproductos}` );
-
-    producto[0]=1500;
-    producto[1]=1000;
-    producto[2]=2000;
-    producto[3]=3000;
-
-    
-
-switch (ropa) { 
+  switch (ropa) {
     case "1":
+      console.log(`Pantalon, valor ${producto[0].precio}`);
+      suma = sumar(total, producto[0].precio);
 
-        console.log (`Pantalon, valor ${producto[0]}`);
-        suma= sumar(total ,producto[0]);
-
-        carrito.push("Pantalon");
-        
+      carrito.push("pantalon");
       break;
 
     case "2":
+      console.log(`Remera, valor ${producto[1].precio}`);
+      suma = sumar(total, producto[1].precio);
 
-        console.log(`Remera, valor ${producto[1]}`);
-        suma=sumar(total ,producto[1]);
-
-        carrito.push("Remera");
-     break;
+      carrito.push("remera");
+      break;
 
     case "3":
+      console.log(`Buzo, valor ${producto[2].precio}`);
+      suma = sumar(total, producto[2].precio);
 
-        console.log (`Buzo, valor ${producto[2]}`);
-        suma= sumar(total ,producto[2]);
+      carrito.push("buzo");
+      break;
 
-        carrito.push("Buzo");
-        
+    case "4":
+      console.log(`zapatillas, valor ${producto[3]}`);
+      suma = sumar(total, producto[3].precio);
 
+      carrito.push("zapatillas");
+      break;
 
-     break;
-
-     case "4":
-
-        console.log (`zapatillas, valor ${producto[3]}`);
-        suma= sumar(total ,producto[3]);
-
-        carrito.push("Zapatillas");
-        
-
-
-        break;
-
-
-        default:
-            console.log ("no tenemos ese producto");
-        }
-
-        console.log(`Productos selecionados:\n${carrito.join("\n")}`);
-
-
-
-        total=suma;
-
-       
-       
-        console.log (`Total: ${total}`);
-       
-
-
-
-            entrada= prompt ("ingrese + si quiere seguir comprando si quiere finalizar indique fin");
-
-            while( entrada != "+" && entrada != "fin"){
-                entrada= prompt ( "el valor ingresado es invalido ingrese + para seguir sumando o fin para finalizar")
-            }
-
-  }while (entrada != "fin");
-
-  
-
-  if (total == 0){
-
-    console .log ("No seleciono ningun producto")
+    default:
+      console.log("no tenemos ese producto");
   }
-  else{
-    
 
-  console.log (`El precio que tiene que abonar es:  ${total}. \n Por los productos : ${carrito.join("\n")} `);
+  console.log(`Productos selecionados:\n${carrito.join("\n")}`);
 
-  pago=prompt ("Como va a abonar credito; debito o efectivo");
+  total = suma;
 
-  while( pago != "credito" && pago != "debito" && pago != "efectivo"){
-    pago= prompt ( "el valor ingresado es invalido ingrese credito; debito o efectivo")
+  console.log(`Total: ${total}`);
+
+  entrada = prompt("ingrese + si quiere seguir comprando si quiere finalizar indique fin");
+
+  while (entrada != "+" && entrada != "fin") {
+    entrada = prompt("el valor ingresado es invalido ingrese + para seguir sumando o fin para finalizar");
+  }
+
+} while (entrada != "fin");
+
+if (total == 0) {
+  console.log("No seleciono ningun producto");
+} else {
+  console.log(`Carrito :\n ${carrito.join("\n")}`);
+
+  let consulta = prompt("Si desea eliminar un producto del carrito indique si en caso que no lo desee indique no");
+
+  while (consulta != "si" && consulta != "no") {
+    consulta = prompt("el valor ingresado es invalido ingrese si para eliminar o no para continuar con su compra");
+  }
+
+  if (consulta == "si") {
+    do {
+      let eliminar = (baja) => {
+        let i = carrito.indexOf(baja);
+
+        if (i != -1) {
+          carrito.splice(i, 1);
+        }
+      };
+
+      productoaeliminar = prompt(`indique el nombre del producto que desea eliminar \n${carrito.join("\n")}`);
+
+      if (productoaeliminar == "pantalon" && carrito.indexOf("pantalon") > -1) {
+        eliminar(productoaeliminar);
+        resta = restar(total, producto[0].precio);
+
+      } else if (productoaeliminar == "remera" && carrito.indexOf("remera") > -1) {
+        eliminar(productoaeliminar);
+        resta = restar(total, producto[1].precio);
+
+      } else if (productoaeliminar == "buzo" && carrito.indexOf("buzo") > -1) {
+        eliminar(productoaeliminar);
+        resta = restar(total, producto[2].precio);
+
+      } else if (productoaeliminar == "zapatillas" && carrito.indexOf("zapatillas") > -1) {
+        eliminar(productoaeliminar);
+        resta = restar(total, producto[3].precio);
+
+      } else {
+        console.log("El producto ingresado no se encuentra en el carrito");
+      }
+
+      console.log(`Carrito :\n${carrito.join("\n")}`);
+
+      total = resta;
+
+      console.log(`Total: ${total}`);
+
+      entrada = prompt(
+        " indique si para eliminar otro producto  o no para finalizar la compra"
+      );
+
+      while (entrada != "si" && entrada != "no") {
+        entrada = prompt("el valor ingresado es invalido indique si para eliminar otro producto  o no para finalizar la compra");
+      }
+      
+    } while (entrada != "no");
+  } else {
+    console.log("no elimino ningun producto");
+  }
+
+  if (total > 0) {
+    console.log(
+      `El precio que tiene que abonar es:  ${total}. \n Carrito :\n ${carrito.join(
+        "\n"
+      )} `
+    );
+
+    pago = prompt("Como va a abonar credito; debito o efectivo");
+
+    while (pago != "credito" && pago != "debito" && pago != "efectivo") {
+      pago = prompt(
+        "el valor ingresado es invalido ingrese credito; debito o efectivo"
+      );
+    }
+
+    if (pago == "credito") {
+      console.log(
+        `El pago de ${total} con tarjeta de credito se realizo correctamente`);
+
+    } else if (pago == "debito") {
+      console.log( `El pago de ${total} con tarjeta de debito se realizo correctamente`);
+
+    } else {
+      console.log(`El pago de ${total} con efectivo se realizo correctamente`);
+    }
+
+  } else {
+    console.log("no tiene productos en el carrito");
+  }
 }
 
-  if (pago == "credito"){
-
-    console.log (`El pago de ${total} con tarjeta de credito se realizo correctamente`);
-
-  }
-  else if (pago == "debito"){
-
-    console.log (`El pago de ${total} con tarjeta de debito se realizo correctamente`);
-  }
-  else {
-
-    console.log (`El pago de ${total} con efectivo se realizo correctamente`);
-  }
-  
-
-
-  }
- 
-
- 
-  
-
-
 /*----------------------------------------------------------------------------------------*/
-  function sumar(sum1, sum2){
+function sumar(sum1, sum2) {
+  var resultado;
 
+  resultado = parseInt(sum1) + parseInt(sum2);
 
-    var resultado;
+  return resultado;
+}
 
+/*-----------------------------------------------------------------------------------*/
+function restar(res1, res2) {
+  var resultado;
 
-    resultado = parseInt(sum1) + parseInt (sum2);
+  resultado = parseInt(res1) - parseInt(res2);
 
-
-    return resultado;
-
-
+  return resultado;
 }
